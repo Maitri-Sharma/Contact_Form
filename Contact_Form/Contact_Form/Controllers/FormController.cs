6c35a5e2a0df74c6596a12cdbc87f9243d659a23
maitri-sharma <Maitri.Sharma@posten.no>
2025-05-06 16:29:26 +0200

    add contact_form folder

---
+++ Contact_Form/Contact_Form/Controllers/FormController.cs
@@ -0,0 +1,39 @@
+﻿using Contact_Form.Data;
+using Contact_Form.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact_Form.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FormController : Controller
+    {
+        private readonly AppDbContext _context;
+        public FormController(AppDbContext context) => _context = context;
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers() =>
+        Ok(await _context.Forms.ToListAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(Form form)
+        {
+            var lastForm = await _context.Forms
+                                  .OrderByDescending(f => f.Id)
+                                  .FirstOrDefaultAsync();
+
+            form.Id = lastForm != null ? lastForm.Id + 1 : 1;
+
+            _context.Forms.Add(form);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(CreateUser), new { id = form.Id }, form);
+        }
+    }
+}
